60a530f57a323822c93ccf5ca565071e8ce8dd8c
leandro correia <leandronunescorreia@outlook.com>
2025-04-13 11:32:17 -0300

    compilation works on local windows machine

--- nncpp/nncpp.cpp
+++ nncpp/nncpp.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
-#include <Eigen/Dense>
+#include <opencv2/opencv.hpp>
 
-using Eigen::MatrixXd;
+#include <Eigen/Dense>
 
 
 int main() {
-    std::cout << "First compilation!\n";
+   std::cout << "First compilation!\n";
+   std::cout << "OpenCV version is " << CV_VERSION << std::endl;
 
-    MatrixXd m(2, 2);
-    m(0, 0) = 3;
-    m(1, 0) = 2.5;
-    m(0, 1) = -1;
-    m(1, 1) = m(1, 0) + m(0, 1);
-    std::cout << m << std::endl;
+   Eigen::MatrixXd m(2, 2);
+   m(0, 0) = 3;
+   m(1, 0) = 2.5;
+   m(0, 1) = -1;
+   m(1, 1) = m(1, 0) + m(0, 1);
+   std::cout << m << std::endl;
+
+   cv::Mat mat(2, 2, CV_64F); // Corrigido para usar o namespace cv corretamente
 }
